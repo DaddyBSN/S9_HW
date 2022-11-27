2dd4c48e30b9a58c0845f9f17e015f197a21de53
Daddy <sergoffan@mail.ru>
2022-11-27 21:19:00 +0300

    №3 solved

--- HW_03/Program.cs
+++ HW_03/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу вычисления функции Аккермана
+
+Console.WriteLine("Введите число M");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+int AckermanFunc(int numM, int numN)
+{
+    if (numM == 0)
+        return numN + 1;
+    if (numM > 0 && numN == 0)
+        return AckermanFunc(numM - 1, 1);
+    else
+        return AckermanFunc(numM - 1, AckermanFunc(numM, numN - 1));
+}
+Console.WriteLine($"A({numM}, {numN}) = {AckermanFunc(numM, numN)}");
