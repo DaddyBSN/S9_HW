1f50d89a763e2c60d5b1cbcba192aaf62f1ded9f
Daddy <sergoffan@mail.ru>
2022-11-27 21:31:49 +0300

    №4 done

--- HW_04/Program.cs
+++ HW_04/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Введите первое целое число: ");
+int numFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int numSecond = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество целых чисел: ");
+int numAmount = Convert.ToInt32(Console.ReadLine());
+
+void numSumm(int numFirst, int numSecond, int numAmount)
+{
+    if (N <= 1) Console.Write($", {numFirst + numSecond}");
+    else
+    {
+        Console.Write($", {numFirst + numSecond}");
+        numSumm(numSecond, numFirst + numSecond, numAmount - 1);
+    }
+}
+
+
+Console.Write($"{numFirst}, {numSecond}");
+numSumm(numFirst, numSecond, numAmount);
