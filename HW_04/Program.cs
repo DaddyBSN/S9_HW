b7bc4e7fc7761a40d3bab7073ec4ef643c64be61
Daddy <sergoffan@mail.ru>
2022-11-27 21:37:57 +0300

    №4 fix

--- HW_04/Program.cs
+++ HW_04/Program.cs
@@ -1,4 +1,8 @@
-﻿
+﻿/*
+Написать программу показывающие первые N чисел, 
+для которых каждое следующее равно сумме двух предыдущих. 
+Первые два элемента последовательности задаются пользователем
+*/
 
 Console.WriteLine("Введите первое целое число: ");
 int numFirst = Convert.ToInt32(Console.ReadLine());
@@ -9,14 +13,14 @@ int numAmount = Convert.ToInt32(Console.ReadLine());
 
 void numSumm(int numFirst, int numSecond, int numAmount)
 {
-    if (N <= 1) Console.Write($", {numFirst + numSecond}");
+    if (numAmount <= 1) 
+        Console.Write($", {numFirst + numSecond}");
     else
     {
         Console.Write($", {numFirst + numSecond}");
         numSumm(numSecond, numFirst + numSecond, numAmount - 1);
     }
 }
-
 
 Console.Write($"{numFirst}, {numSecond}");
 numSumm(numFirst, numSecond, numAmount);
