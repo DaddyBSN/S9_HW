76f2dd87a409194f4850617b8ad37bb0ceaa737f
Daddy <sergoffan@mail.ru>
2022-11-27 14:37:23 +0300

    №1 solved

--- HW_01/Program.cs
+++ HW_01/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+Console.WriteLine("Введите число N");
+int numN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число M");
+int numM = Convert.ToInt32(Console.ReadLine());
+
+if (numM > numN)
+    Console.WriteLine("Число M не может быть больше N");
+else
+{
+    PrintNumbersToN(numN, numM);
+}
+
+void PrintNumbersToN(int N, int M)
+{
+    if (N < M) return;
+    PrintNumbersToN(N - 1, M);
+    Console.Write(N + " ");
+}
